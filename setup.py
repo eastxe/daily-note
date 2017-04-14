d516dd50c6b97d50f81469aa5cbc9fa315d3c092
eastxe <eastxe@gmail.com>
2017-04-14 15:33:58 +0900

    fixed

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup
 setup(
         name='daily-note',
         version='0.2',
+        py_modules=['note'],
         install_requires=[
             'click', ],
         entry_points='''
